9840b4b9102f971563a30fc6683a18b4f523a316
djl1626 <djl47@uw.edu>
2022-04-28 17:30:02 -0700

    added the fruits picker

--- streamline_app.py
+++ streamline_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas as pd
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_list = fruit_list.set_index('Fruit')
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -9,4 +10,5 @@ streamlit.text("🥗 Kale, Spinach, & Rocket Smoothie")
 streamlit.text("🐔 Hard-boiled Free-range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+streamlit.multiselect("Pick some fruits:", list(fruit_list.index))
 streamlit.dataframe(fruit_list)
