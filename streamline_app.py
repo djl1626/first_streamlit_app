f840abc97fa1301d7932ac332fd0e81e59f1d088
djl1626 <djl47@uw.edu>
2022-04-28 17:27:43 -0700

    added the fruits dataframe

--- streamline_app.py
+++ streamline_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas as pd
+fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -7,3 +9,4 @@ streamlit.text("🥗 Kale, Spinach, & Rocket Smoothie")
 streamlit.text("🐔 Hard-boiled Free-range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+streamlit.dataframe(fruit_list)
